79c2f88ba5a1297a7e5eb9da2e2cd09b2635d8ba
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-26 22:40:48 -0400

    add delete note to server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const PORT = process.env.port || 3001;
-const database = './db/db.json'
 
 const app = express();
 app.use(express.json());
@@ -27,17 +27,26 @@ app.get('/api/notes', (req, res) => {
     })
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 app.post('/api/notes', (req, res) => {
-    let note = req.body;
+    let {title, text} = req.body;
+    let note = {
+        id: uuidv4(),
+        title,
+        text
+    };
     console.log(note);
     fs.readFile("./db/db.json", (err, data) => {
         if(err){
             console.log("could not read file");
         }
         else{
-            let tempNotes = JSON.parse(data);
-            tempNotes.push(note);
-            fs.writeFile("./db/db.json", JSON.stringify(tempNotes), (err)=> {
+            let tempNote = JSON.parse(data);
+            tempNote.push(note);
+            fs.writeFile("./db/db.json", JSON.stringify(tempNote, null, 2), (err)=> {
                 if(err){
                     console.log("error occured");
                 }
@@ -51,6 +60,25 @@ app.post('/api/notes', (req, res) => {
     
 })
 
+app.delete('/api/notes/:id', (req,res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if(err){
+            console.log("could not read file");
+        }
+        else{
+            let tempNote = JSON.parse(data);
+            tempNote.splice(tempNote.indexOf(req.params.id),1);
+            fs.writeFile("./db/db.json", JSON.stringify(tempNote, null, 2), (err) => {
+                if(err){
+                    console.log("error occured");
+                }
+                else{
+                    res.json("successfully delete note");
+                }
+            })
+        }
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
